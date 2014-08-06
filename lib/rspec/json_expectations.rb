94805d140ff901c62ab628911a31a3aec1c47d7c
Alexey Fedorov <alexey.fedorov@wimdu.com>
2014-08-06 23:53:14 +0200

    Initial commit

---
+++ lib/rspec/json_expectations.rb
@@ -0,0 +1,7 @@
+require "rspec/json_expectations/version"
+
+module Rspec
+  module JsonExpectations
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/json_expectations/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module JsonExpectations
+    VERSION = "0.0.1"
+  end
+end
