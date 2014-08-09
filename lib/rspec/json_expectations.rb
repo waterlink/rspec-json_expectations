ae65332ccae6bb6c2c9fc9d3e6ed0d70d58e7b03
Alexey Fedorov <alexey.fedorov@wimdu.com>
2014-08-09 16:38:57 +0200

    wip

--- lib/rspec/json_expectations.rb
+++ lib/rspec/json_expectations.rb
@@ -1,6 +1,6 @@
 require "rspec/json_expectations/version"
-require "rspec/json_expectations/expectations"
 require "rspec/json_expectations/exclusion"
+require "rspec/json_expectations/expectations"
 
 module RSpec
   module JsonExpectations
--- lib/rspec/json_expectations/expectations.rb
+++ lib/rspec/json_expectations/expectations.rb
@@ -18,25 +18,33 @@ module RSpec
         called_in = opts.fetch(:called_in)
 
         if json.is_a?(Hash)
+
           json.each do |key, new_json|
             new_prefix = with_prefix + [key.to_s]
             generate_examples_for(new_json, opts.merge(with_prefix: new_prefix))
           end
+
         elsif json.is_a?(Array)
+
           raise NotImplemented.new("Arrays are not allowed yet")
+
         elsif json.is_a?(String) || json.is_a?(Numeric)
+
           it "is expected to have equal value at json[\"#{with_prefix.join('"]["')}\"]" do
             value = JSON.parse(subject)
             with_prefix.each { |key| value = value[key.to_s] }
             begin
               expect(value).to eq(json)
             rescue Exception => e
+              e.backtrace.delete_if { |line| line =~ RSpec::JsonExpectations::EXCLUSION_PATTERN }
               e.backtrace.insert(0, called_in)
               raise e
             end
           end
+
         end
       end
+
     end
   end
 end
