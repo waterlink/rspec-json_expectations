1d67456a2e4eca13319e46d0cc85a074ac071550
esjee <ewfawefw3@gmail.com>
2016-07-01 22:48:51 +0200

    DRY it up

--- lib/rspec/json_expectations/matchers.rb
+++ lib/rspec/json_expectations/matchers.rb
@@ -1,40 +1,4 @@
-RSpec::Matchers.define :include_json do |expected|
-
-  # RSpec 2 vs 3
-  if respond_to?(:failure_message)
-    match do |actual|
-      traverse(expected, actual, false)
-    end
-
-    match_when_negated do |actual|
-      traverse(expected, actual, true)
-    end
-
-    failure_message do |actual|
-       RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-
-    failure_message_when_negated do |actual|
-       RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-  else
-    match_for_should do |actual|
-      traverse(expected, actual, false)
-    end
-
-    match_for_should_not do |actual|
-      traverse(expected, actual, true)
-    end
-
-    failure_message_for_should do |actual|
-      RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-
-    failure_message_for_should_not do |actual|
-      RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-  end
-
+RSpec::JsonExpectations::MatcherFactory.new(:include_json).define_matcher do
   def traverse(expected, actual, negate=false)
     unless expected.is_a?(Hash) ||
         expected.is_a?(Array) ||
@@ -55,43 +19,7 @@ RSpec::Matchers.define :include_json do |expected|
   end
 end
 
-RSpec::Matchers.define :include_unordered_json do |expected|
-
-  # RSpec 2 vs 3
-  if respond_to?(:failure_message)
-    match do |actual|
-      traverse(expected, actual, false)
-    end
-
-    match_when_negated do |actual|
-      traverse(expected, actual, true)
-    end
-
-    failure_message do |actual|
-       RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-
-    failure_message_when_negated do |actual|
-       RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-  else
-    match_for_should do |actual|
-      traverse(expected, actual, false)
-    end
-
-    match_for_should_not do |actual|
-      traverse(expected, actual, true)
-    end
-
-    failure_message_for_should do |actual|
-      RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-
-    failure_message_for_should_not do |actual|
-      RSpec::JsonExpectations::FailurePresenter.render(@include_json_errors)
-    end
-  end
-
+RSpec::JsonExpectations::MatcherFactory.new(:include_unordered_json).define_matcher do
   def traverse(expected, actual, negate=false)
     unless expected.is_a?(Array)
       raise ArgumentError,
