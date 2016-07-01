90f7e189a34cb466efa478b4c9f82aae67712c1f
esjee <ewfawefw3@gmail.com>
2016-07-01 22:49:57 +0200

    amend

--- lib/rspec/json_expectations.rb
+++ lib/rspec/json_expectations.rb
@@ -1,7 +1,5 @@
 require "rspec/core"
 require "rspec/json_expectations/matcher_factory"
-require "rspec/json_expectations/base_matcher"
-require "rspec/json_expectations/include_json_matcher"
 require "rspec/json_expectations/version"
 require "rspec/json_expectations/json_traverser"
 require "rspec/json_expectations/failure_presenter"
