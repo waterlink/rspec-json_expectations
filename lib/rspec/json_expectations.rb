e398e28876c3e34ee95b2ca20fccf0376275525f
Dimas Acuna <dacuna@inss.gob.ni>
2018-04-26 13:45:47 -0600

    Fix error with rspec-rails gem on version 3.5.2

--- lib/rspec/json_expectations.rb
+++ lib/rspec/json_expectations.rb
@@ -1,4 +1,5 @@
 require "rspec/core"
+require "rspec/expectations"
 require "rspec/json_expectations/matcher_factory"
 require "rspec/json_expectations/version"
 require "rspec/json_expectations/json_traverser"
